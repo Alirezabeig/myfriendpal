10ad591613fb149f4f037cf40da7293fabd2f7f1
alirezabeig <alirezabeig@Alireza-Beig.local>
2023-09-25 16:19:21 -0700

    updated for sendin custom messages

--- app.py
+++ app.py
@@ -1,72 +1,32 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from twilio.rest import Client
-import requests
-import os  # For fetching environment variables
-from dotenv import load_dotenv  # Required to load variables from .env file
+import os
+from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 
-# Fetching environment variables
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-GPT4_API_ENDPOINT = os.getenv('GPT4_API_ENDPOINT')
-GPT4_API_KEY = os.getenv('GPT4_API_KEY')
+TWILIO_ACCOUNT_SID = 'AC4f83e220b05a9e196c601e69705b44ab'
+TWILIO_AUTH_TOKEN = 'fae80af5822f21e3e00544462caabe3d'
+TWILIO_PHONE_NUMBER = '+18666421882'
 
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 @app.route('/')
 def index():
-    return render_template('ui.html')
+    return render_template('index.html')
 
-@app.route('/sendSMS', methods=['POST'])
-def send_sms():
-    phone_number = request.json.get('phoneNumber')
-    try:
-        message = client.messages.create(
-            body="Please reply with your name.",
-            from_=TWILIO_PHONE_NUMBER,
-            to=phone_number
-        )
-        return jsonify({"message": "SMS sent!"})
-    except Exception as e:
-        return jsonify({"message": "Failed to send SMS.", "error": str(e)}), 500
-
-@app.route('/receiveSMS', methods=['POST'])
-def receive_sms():
-    incoming_message = request.form.get('Body')
-    
-    headers = {
-        "Authorization": f"Bearer {GPT4_API_KEY}",
-        "Content-Type": "application/json"
-    }
-    
-    data = {
-        "model": "gpt-4",  # Specifying the model
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": incoming_message}
-        ]
-    }
-    
-    # Note: The `GPT4_API_ENDPOINT` should now be the base URL.
-    try:
-        response = requests.post(f"{GPT4_API_ENDPOINT}/v1/chat/completions", headers=headers, json=data)
-        response.raise_for_status()
-        gpt_response = response.json()["choices"][0]["message"]["content"].strip()
-
-        message = client.messages.create(
-            body=gpt_response,
-            from_=TWILIO_PHONE_NUMBER,
-            to=request.form.get('From')
-        )
-        return "OK", 200
-    except Exception as e:
-        return str(e), 500
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    data = request.json
+    phone_number = data.get('phone_number')
+    message = client.messages.create(
+        to=phone_number,
+        from_=TWILIO_PHONE_NUMBER,
+        body="hey, very nice meeting you!"
+    )
+    return jsonify({'message': 'Message sent!'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,16 +1,30 @@
-document.getElementById('phoneNumberForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  let phoneNumber = document.getElementById('phoneNumber').value;
-  
-  fetch('/sendSMS', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ phoneNumber: phoneNumber })
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.message);
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('phoneNumberForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let phoneNumber = document.getElementById('phoneNumber').value;
+
+    fetch('/send_message', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ phone_number: phoneNumber })
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.message);
+        alert(data.message);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Failed to send SMS. Please try again.');
+      });
   });
 });
+
