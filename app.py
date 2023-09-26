b8cc5deab198fb570f8376406eebdc7d142ae763
alirezabeig <alirezabeig@Alireza-Beig.local>
2023-09-26 10:49:23 -0700

    added gpt greating

--- app.py
+++ app.py
@@ -12,7 +12,21 @@ TWILIO_PHONE_NUMBER = '+18666421882'
 
 
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+GPT4_API_KEY = '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'
 
+openai.api_key = GPT4_API_KEY
+
+def generate_greeting():
+    try:
+        response = openai.Completion.create(
+          engine="text-davinci-002",
+          prompt="Create a friendly greeting message.",
+          max_tokens=50
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        logging.error(f"Failed to generate message with GPT-4: {e}")
+        return "Hey there, nice to meet you!"
 
 @app.route('/')
 def index():
@@ -23,16 +37,20 @@ def send_message():
     try:
         data = request.json
         phone_number = data.get('phone_number')
+        
+        greeting_message = generate_greeting()
+        
         message = client.messages.create(
             to=phone_number,
             from_=TWILIO_PHONE_NUMBER,
-            body="hey, very nice meeting you!"
+            body=greeting_message
         )
         logging.info(f"Message sent with ID: {message.sid}")
         return jsonify({'message': 'Message sent!'})
     except Exception as e:
         logging.error(f"Failed to send message: {e}")
         return jsonify({'message': 'Failed to send message', 'error': str(e)})
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))  # Fetch the port from environment variables or set to 5000
