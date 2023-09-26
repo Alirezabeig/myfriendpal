ef6f403e5051769b7d7b38b396c0e2f1d26fbb33
alirezabeig <alirezabeig@Alireza-Beig.local>
2023-09-26 15:20:05 -0700

    newoopp

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('phoneNumberForm').addEventListener('submit', function (e) {
-      console.log("form submitted")
     e.preventDefault();
 
     let phoneNumber = document.getElementById('phoneNumber').value;
@@ -12,20 +11,9 @@ document.addEventListener('DOMContentLoaded', function () {
       },
       body: JSON.stringify({ phone_number: phoneNumber })
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data.message);
-        alert(data.message);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        alert('Failed to send SMS. Please try again.');
-      });
+      .then(response => response.json())
+      .then(data => alert(data.message))
+      .catch(error => alert('Failed to send SMS. Please try again.'));
   });
 });
 
