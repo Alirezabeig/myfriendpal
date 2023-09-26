95f4bc0d52cd6cea5e1e5d147b242fefc595d6e0
alirezabeig <alirezabeig@Alireza-Beig.local>
2023-09-26 09:49:03 -0700

    added gpt4

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from twilio.rest import Client
 import os
 from dotenv import load_dotenv
 import logging
+import openai
 
 app = Flask(__name__)
 
@@ -10,7 +11,22 @@ TWILIO_ACCOUNT_SID = 'AC4f83e220b05a9e196c601e69705b44ab'
 TWILIO_AUTH_TOKEN = 'fae80af5822f21e3e00544462caabe3d'
 TWILIO_PHONE_NUMBER = '+18666421882'
 
+GPT4_API_KEY = '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'
+
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+openai.api_key = GPT4_API_KEY
+
+def generate_greeting():
+    try:
+        response = openai.Completion.create(
+          engine="text-davinci-002",
+          prompt="Create a friendly greeting message.",
+          max_tokens=50
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        logging.error(f"Failed to generate message with GPT-4: {e}")
+        return "Hey there, nice to meet you!"
 
 @app.route('/')
 def index():
@@ -21,10 +37,13 @@ def send_message():
     try:
         data = request.json
         phone_number = data.get('phone_number')
+        
+        greeting_message = generate_greeting()
+        
         message = client.messages.create(
             to=phone_number,
             from_=TWILIO_PHONE_NUMBER,
-            body="hey, very nice meeting you!"
+            body=greeting_message
         )
         logging.info(f"Message sent with ID: {message.sid}")
         return jsonify({'message': 'Message sent!'})
@@ -35,4 +54,3 @@ def send_message():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))  # Fetch the port from environment variables or set to 5000
     app.run(host="0.0.0.0", port=port)  # Run the app
-
