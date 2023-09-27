f91a7c8b09625f5479427000c6de8f4c7ef3eb51
alirezabeig <alirezabeig@Alireza-Beig.local>
2023-09-27 09:35:45 -0700

    fal

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log("DOM Content Loaded"); // Debug line
+  console.log("DOM Content Loaded");  // Debug line
 
   const activatePalDiv = document.getElementById('activatePal');
 
   if (activatePalDiv) {
-    console.log("Found Activate Pal div"); // Debug line
+    console.log("Found Activate Pal div");  // Debug line
     activatePalDiv.addEventListener('click', function (e) {
-      console.log("Clicked Activate Pal"); // Debug line
+      console.log("Clicked Activate Pal");  // Debug line
       e.preventDefault();
 
       const phoneNumber = document.getElementById('phoneInput').value;
@@ -23,6 +23,6 @@ document.addEventListener('DOMContentLoaded', function () {
       .catch(error => alert('Failed to send SMS. Please try again.'));
     });
   } else {
-    console.log("Could not find Activate Pal div"); // Debug line
+    console.log("Could not find Activate Pal div");  // Debug line
   }
 });
