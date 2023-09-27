7232b5bc38dd95bc07e82a0728b6b7cdf24fb6d6
alirezabeig <alirezabeig@Alireza-Beig.local>
2023-09-27 09:29:38 -0700

    fal

--- static/script.js
+++ static/script.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+  console.log("DOM Content Loaded"); // Debug line
 
-  // Existing code for auto-moving to next input box
-  // ... (if needed) ...
+  const activatePalDiv = document.getElementById('activatePal');
 
-  // Add this event listener
-  document.getElementById('activatePal').addEventListener('click', function (e) {
-    e.preventDefault();
+  if (activatePalDiv) {
+    console.log("Found Activate Pal div"); // Debug line
+    activatePalDiv.addEventListener('click', function (e) {
+      console.log("Clicked Activate Pal"); // Debug line
+      e.preventDefault();
 
-    // Replace this section with how you want to fetch the phone number
-    const phoneNumber = document.getElementById('phoneInput').value;
+      const phoneNumber = document.getElementById('phoneInput').value;
 
-    fetch('/send_message', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ phone_number: phoneNumber })
-    })
-    .then(response => response.json())
-    .then(data => alert(data.message))
-    .catch(error => alert('Failed to send SMS. Please try again.'));
-  });
-
+      fetch('/send_message', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ phone_number: phoneNumber })
+      })
+      .then(response => response.json())
+      .then(data => alert(data.message))
+      .catch(error => alert('Failed to send SMS. Please try again.'));
+    });
+  } else {
+    console.log("Could not find Activate Pal div"); // Debug line
+  }
 });
