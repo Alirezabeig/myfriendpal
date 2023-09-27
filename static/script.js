1501c67bec6b4364b24ec2db13bfd2980f8b3656
alirezabeig <alirezabeig@Alireza-Beig.local>
2023-09-26 20:34:04 -0700

    fal

--- static/script.js
+++ static/script.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  // Additional code for auto-moving to next input box
+  for (let i = 1; i <= 10; i++) {
+    const inputBox = document.getElementById(`input${i}`);
+    inputBox.addEventListener('input', function () {
+      if (i < 10) {
+        document.getElementById(`input${i + 1}`).focus();
+      }
+    });
+  }
+
+  // Your existing code for form submission
   document.getElementById('phoneNumberForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
-      let phoneNumber = document.getElementById('phoneInput').value;
-
+    // Combine all the single numbers from the 10 input boxes to make the full phone number
+    let phoneNumber = '';
+    for (let i = 1; i <= 10; i++) {
+      phoneNumber += document.getElementById(`input${i}`).value;
+    }
 
     fetch('/send_message', {
       method: 'POST',
@@ -17,4 +32,3 @@ document.addEventListener('DOMContentLoaded', function () {
       .catch(error => alert('Failed to send SMS. Please try again.'));
   });
 });
-
