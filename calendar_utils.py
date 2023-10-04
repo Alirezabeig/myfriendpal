0900c61efd6ac2cb4d1ab254aa24fe630810b0c7
Alireza Beig <alirezabeig@gmail.com>
2023-10-04 12:00:07 -0700

    organizing

--- calendar_utils.py
+++ calendar_utils.py
@@ -1,38 +1,37 @@
 
 # calendar_utils.py
+from googleapiclient.discovery import build
+import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
-import os
-import logging
 
-logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler()])
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar.events.readonly']
 
 def get_calendar_service():
-    # Load the saved credentials
     creds = None
     if os.path.exists('token.json'):
         with open('token.json', 'r') as token_file:
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-    # If we have valid credentials, proceed to call Google Calendar APIs
     if creds and not creds.expired:
         return build('calendar', 'v3', credentials=creds)
     else:
         return None
-        
+
 def initialize_google_calendar():
-    """Initialize the Google Calendar API and return Auth URL."""
-    logging.info("Initializing Google Calendar")
-    try:
-        ##SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-        flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
-        auth_url, _ = flow.authorization_url()
-        
-        logging.info(f"Auth URL generated: {auth_url}")
-        return auth_url
-    except Exception as e:
-        logging.error(f"Failed to initialize Google Calendar: {e}")
-        return None
+    client_id = "1084838804894-s7bra6uila2ffshf1712qnb9lf2hk781.apps.googleusercontent.com"
+    state_string = "some_random_string"
+    redirect_uri = "https://www.myfriendpal.com/oauth2callback"
+
+    auth_url = f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={client_id}&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly&state={state_string}&access_type=offline&redirect_uri={redirect_uri}"
+
+    return auth_url
+    
+def authorize_google_calendar():
+    app.logger.info('Google Calendar authorization')
+    flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+    with open(CALENDAR_CREDENTIALS_FILE, 'w') as token:
+        token.write(creds.to_json())
+    
+    return "Google Calendar integration successful! You can now go back to your chat."
