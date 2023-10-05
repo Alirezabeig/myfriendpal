2366d9866bf54d69da549568b5dab135c20e82cd
Alireza Beig <alirezabeig@gmail.com>
2023-10-05 13:33:59 -0700

    t

--- calendar_utils.py
+++ calendar_utils.py
@@ -6,25 +6,23 @@ import os
 
 # Set up API credentials
 
-CALENDAR_CREDENTIALS_FILE = "client_secret.json" 
+CALENDAR_CREDENTIALS_FILE = "client_secret.json"
 CALENDAR_API_SERVICE_NAME = 'calendar'
 CALENDAR_API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-def get_google_calendar_service():
+def get_google_calendar_authorization_url():
     flow = InstalledAppFlow.from_client_secrets_file(
         CALENDAR_CREDENTIALS_FILE, SCOPES)
-
-    # Direct the user to the authorization URL
-    auth_url, _ = flow.authorization_url(
-        'https://accounts.google.com/o/oauth2/auth',
+    authorization_url, _ = flow.authorization_url(
+        "https://accounts.google.com/o/oauth2/auth",
         access_type='offline',
         include_granted_scopes='true')
     
-    return auth_url
+    return authorization_url
 
-def fetch_events(service, calendar_id='primary'):
-    events_result = service.events().list(calendarId=calendar_id, singleEvents=True,
-                                           orderBy='startTime').execute()
+def fetch_calendar_events(credentials):
+    service = build("calendar", "v3", credentials=credentials)
+    events_result = service.events().list(calendarId='primary').execute()
     events = events_result.get('items', [])
     return events
