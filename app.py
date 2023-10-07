d9924b3bfe077a756e8c23b7cc8b819e08d9549a
Alireza Beig <alirezabeig@gmail.com>
2023-10-06 17:25:51 -0700

    separeted db.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ import os
 from dotenv import load_dotenv
 import logging
 import json
+
 from config import load_configurations
+from db import create_connection
+
 import openai
 import psycopg2
 from psycopg2 import OperationalError
@@ -97,34 +100,6 @@ def oauth2callback():
 
     return "Authorization complete..."
 
-def create_connection():
-    print("Inside create_connection function and it is kicking")
-    try:
-        db_host = os.environ.get("DB_HOST")
-        db_port = os.environ.get("DB_PORT")
-        db_user = os.environ.get("DB_USER")
-        db_password = os.environ.get("DB_PASSWORD")
-        db_name = os.environ.get("DB_NAME")
-        print("Attempting to connect to: host={db_host} port={db_port} user={db_user} dbname={db_name}")
-        
-        connection = psycopg2.connect(
-            host=db_host,
-            port=db_port,
-            user=db_user,
-            password=db_password,
-            database=db_name
-        )
-        create_table(connection)
-        print("Connection created ***",connection)
-        return connection
-        
-    except OperationalError as oe:
-        print(f"An OperationalError occurred: {oe}")
-        logging.error(f"The full error is: {oe}")
-    except Error as e:
-        print(f"An explicit error occurred: {e}")
-        logging.error(f"The full error is: {e}")
-        
 def create_table(connection):
     print("Inside create_table()")
     try:
---
+++ db.py
@@ -0,0 +1,33 @@
+# db.py
+import os
+import psycopg2
+from psycopg2 import OperationalError, Error
+
+def create_connection():
+    print("Inside create_connection function and it is kicking")
+    try:
+        db_host = os.environ.get("DB_HOST")
+        db_port = os.environ.get("DB_PORT")
+        db_user = os.environ.get("DB_USER")
+        db_password = os.environ.get("DB_PASSWORD")
+        db_name = os.environ.get("DB_NAME")
+        print("Attempting to connect to: host={db_host} port={db_port} user={db_user} dbname={db_name}")
+        
+        connection = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        create_table(connection)
+        print("Connection created ***",connection)
+        return connection
+        
+    except OperationalError as oe:
+        print(f"An OperationalError occurred: {oe}")
+        logging.error(f"The full error is: {oe}")
+    except Error as e:
+        print(f"An explicit error occurred: {e}")
+        logging.error(f"The full error is: {e}")
+        
