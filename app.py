892fdf1d11a205bb16977365253ec2f2ea52629f
Alireza Beig <alirezabeig@gmail.com>
2023-10-06 17:55:46 -0700

    t

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import json
 
 from config import load_configurations
 from db import create_connection
+from google_calendar import oauth2callback, get_new_access_token
 
 import openai
 import psycopg2
@@ -58,48 +59,6 @@ def check_for_calendar_keyword(user_input, phone_number):
     else:
         print("Calendar keyword not found.")  # Debug line
         return False
-
-@app.route('/oauth2callback', methods=['GET'])
-def oauth2callback():
-    print("OAUTH2CALLBACK &*&*## -- ##")
-    auth_code = request.args.get('code')
-    phone_number = request.args.get('state')  # Assuming you passed phone_number as 'state' during OAuth2 initiation
-
-    token_data = {
-        'client_id': GOOGLE_CLIENT_ID,
-        'client_secret': GOOGLE_CLIENT_SECRET,
-        'redirect_uri': 'https://www.myfriendpal.com/oauth2callback',
-        'code': auth_code,
-        'grant_type': 'authorization_code'
-    }
-
-    response = requests.post('https://oauth2.googleapis.com/token', data=token_data)
-    token_info = response.json()
-
-    # Save the access token and refresh token somewhere secure for future use
-    access_token = token_info.get('access_token')
-    refresh_token = token_info.get('refresh_token')
-
-    # Fetch and store Gmail ID and next Google Calendar event
-    google_calendar_email, next_event = fetch_google_calendar_info(access_token, refresh_token)
-
-    # Check if connection is closed
-    if conn.closed:
-        print("Connection closed, re-opening...")
-        # Re-open your connection here
-        # conn = psycopg2.connect( ... )
-    
-    # Update the database
-    with conn.cursor() as cursor:  # Using 'with' ensures the cursor will be closed after use
-        update_query = '''UPDATE conversations SET oauth_token = %s, google_calendar_email = %s, next_event = %s, refresh_token = %s WHERE phone_number = %s;'''
-        try:
-            cursor.execute(update_query, (json.dumps(token_info), google_calendar_email, next_event, refresh_token, phone_number))
-            conn.commit()
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            conn.rollback()
-
-    return "Authorization complete..."
 
 def generate_response(user_input, phone_number):
     print("inside_generate response")
@@ -224,22 +183,6 @@ def send_message():
         logging.error(f"Failed to send message: {e}")
         return jsonify({'message': 'Failed to send message', 'error': str(e)})
     
-def get_new_access_token(refresh_token):
-    data = {
-        'client_id': GOOGLE_CLIENT_ID,
-        'client_secret': GOOGLE_CLIENT_SECRET,
-        'refresh_token': refresh_token,
-        'grant_type': 'refresh_token'
-    }
-    try:
-        response = requests.post('https://oauth2.googleapis.com/token', data=data)
-        token_info = response.json()
-        new_access_token = token_info.get('access_token')
-        return new_access_token
-    except Exception as e:
-        logging.error(f"Failed to get new access token: {e}")
-        return None
-
 if __name__ == '__main__':
     print("Script is starting")
     app.debug = True
---
+++ google_calendar.py
@@ -0,0 +1,64 @@
+
+# google_calender.py
+from flask import request
+import requests
+import os
+from db import conn
+
+@app.route('/oauth2callback', methods=['GET'])
+def oauth2callback():
+    print("OAUTH2CALLBACK &*&*## -- ##")
+    auth_code = request.args.get('code')
+    phone_number = request.args.get('state')  # Assuming you passed phone_number as 'state' during OAuth2 initiation
+
+    token_data = {
+        'client_id': GOOGLE_CLIENT_ID,
+        'client_secret': GOOGLE_CLIENT_SECRET,
+        'redirect_uri': 'https://www.myfriendpal.com/oauth2callback',
+        'code': auth_code,
+        'grant_type': 'authorization_code'
+    }
+
+    response = requests.post('https://oauth2.googleapis.com/token', data=token_data)
+    token_info = response.json()
+
+    # Save the access token and refresh token somewhere secure for future use
+    access_token = token_info.get('access_token')
+    refresh_token = token_info.get('refresh_token')
+
+    # Fetch and store Gmail ID and next Google Calendar event
+    google_calendar_email, next_event = fetch_google_calendar_info(access_token, refresh_token)
+
+    # Check if connection is closed
+    if conn.closed:
+        print("Connection closed, re-opening...")
+        # Re-open your connection here
+        # conn = psycopg2.connect( ... )
+    
+    # Update the database
+    with conn.cursor() as cursor:  # Using 'with' ensures the cursor will be closed after use
+        update_query = '''UPDATE conversations SET oauth_token = %s, google_calendar_email = %s, next_event = %s, refresh_token = %s WHERE phone_number = %s;'''
+        try:
+            cursor.execute(update_query, (json.dumps(token_info), google_calendar_email, next_event, refresh_token, phone_number))
+            conn.commit()
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            conn.rollback()
+
+    return "Authorization complete..."
+    
+def get_new_access_token(refresh_token):
+    data = {
+        'client_id': GOOGLE_CLIENT_ID,
+        'client_secret': GOOGLE_CLIENT_SECRET,
+        'refresh_token': refresh_token,
+        'grant_type': 'refresh_token'
+    }
+    try:
+        response = requests.post('https://oauth2.googleapis.com/token', data=data)
+        token_info = response.json()
+        new_access_token = token_info.get('access_token')
+        return new_access_token
+    except Exception as e:
+        logging.error(f"Failed to get new access token: {e}")
+        return None
