9be57d9201a2a594dd5e72c15aa98f330d8d949c
Alireza Beig <alirezabeig@gmail.com>
2023-10-08 13:11:58 -0700

    g

--- app.py
+++ app.py
@@ -128,11 +128,18 @@ def generate_response(user_input, phone_number, credentials= None):
     google_calendar_email, next_events = None, None  # Initialize to None
 
     if count > 0:
-        cursor.execute("SELECT refresh_token, oauth_token FROM conversations WHERE phone_number = %s", (phone_number,))
-        tokens = cursor.fetchone()
-        if tokens:
-            refresh_token, oauth_token = tokens  # Adjust as per your actual field names
+    cursor.execute("SELECT refresh_token, oauth_token FROM conversations WHERE phone_number = %s", (phone_number,))
+    tokens = cursor.fetchone()
+    if tokens:
+        refresh_token, oauth_token = tokens  # Adjust as per your actual field names
+
+        # Add this check to see if tokens are available
+        if refresh_token and oauth_token:
             google_calendar_email, next_events = fetch_google_calendar_info(refresh_token, oauth_token)
+        else:
+            print("Tokens are not available.")
+
+
     elif credentials:
         google_calendar_email, next_events = fetch_google_calendar_info(credentials)
 
@@ -238,23 +245,7 @@ def send_message():
     except Exception as e:
         logging.error(f"Failed to send message: {e}")
         return jsonify({'message': 'Failed to send message', 'error': str(e)})
-    
-def get_new_access_token(refresh_token):
-    data = {
-        'client_id': GOOGLE_CLIENT_ID,
-        'client_secret': GOOGLE_CLIENT_SECRET,
-        'refresh_token': refresh_token,
-        'grant_type': 'refresh_token'
-    }
-    try:
-        response = requests.post('https://oauth2.googleapis.com/token', data=data)
-        token_info = response.json()
-        new_access_token = token_info.get('access_token')
-        return new_access_token
-    except Exception as e:
-        logging.error(f"Failed to get new access token: {e}")
-        return None
-        
+            
 @app.route('/pal', methods=['GET'])
 def pal_page():
     return render_template('pal.html')
--- google_calendar.py
+++ google_calendar.py
@@ -23,30 +23,34 @@ def oauth2callback():
     response = requests.post('https://oauth2.googleapis.com/token', data=token_data)
     token_info = response.json()
 
-    # Save the access token and refresh token somewhere secure for future use
+    # Check if we have valid tokens
     access_token = token_info.get('access_token')
     refresh_token = token_info.get('refresh_token')
 
-    # Fetch and store Gmail ID and next Google Calendar event
-    google_calendar_email, next_event = fetch_google_calendar_info(access_token, refresh_token)
-
-    # Check if connection is closed
-    if conn.closed:
-        print("Connection closed, re-opening...")
-        # Re-open your connection here
-        # conn = psycopg2.connect( ... )
-    
-    # Update the database
-    with conn.cursor() as cursor:  # Using 'with' ensures the cursor will be closed after use
-        update_query = '''UPDATE conversations SET oauth_token = %s, google_calendar_email = %s, next_event = %s, refresh_token = %s WHERE phone_number = %s;'''
-        try:
-            cursor.execute(update_query, (json.dumps(token_info), google_calendar_email, next_event, refresh_token, phone_number))
-            conn.commit()
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            conn.rollback()
+    if access_token and refresh_token:
+        # Fetch and store Gmail ID and next Google Calendar event
+        google_calendar_email, next_event = fetch_google_calendar_info(access_token, refresh_token)
+        
+        # Check if connection is closed
+        if conn.closed:
+            print("Connection closed, re-opening...")
+            # Re-open your connection here
+            # conn = psycopg2.connect( ... )
+        
+        # Update the database
+        with conn.cursor() as cursor:  # Using 'with' ensures the cursor will be closed after use
+            update_query = '''UPDATE conversations SET oauth_token = %s, google_calendar_email = %s, next_event = %s, refresh_token = %s WHERE phone_number = %s;'''
+            try:
+                cursor.execute(update_query, (json.dumps(token_info), google_calendar_email, next_event, refresh_token, phone_number))
+                conn.commit()
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                conn.rollback()
+    else:
+        print("Failed to get access_token or refresh_token")
 
     return "Authorization complete..."
+
     
 def get_new_access_token(refresh_token):
     data = {
