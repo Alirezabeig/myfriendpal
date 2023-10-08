7459966ad3fc2f3c707c6644d49e9d5b66f90a94
Alireza Beig <alirezabeig@gmail.com>
2023-10-07 17:28:01 -0700

    changed the first message

--- gpt4_utils.py
+++ gpt4_utils.py
@@ -1,20 +1,9 @@
 
 # gpt4_utils.py
-import json
+import os
 import openai
-import logging
+from db import conn
+# Your existing functions related to GPT-4
 
-gpt4_api_key = os.environ.get('GPT4_API_KEY')
-openai.api_key = gpt4_api_key
 
-def get_gpt4_response(conversation):
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=conversation
-        )
-        gpt4_reply = response['choices'][0]['message']['content'].strip()
-        return gpt4_reply
-    except Exception as e:
-        logging.error(f"An error occurred while getting GPT-4 response: {e}")
-        return None
+
