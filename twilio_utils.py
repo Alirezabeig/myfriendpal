3df73ebb0baca073ea034799a1409b5ae9aa82c9
Alireza Beig <alirezabeig@gmail.com>
2023-10-08 13:58:05 -0700

    t

--- twilio_utils.py
+++ twilio_utils.py
@@ -1,9 +1,6 @@
 
 #twilio_utils.py
 from flask import request, jsonify
-from google.oauth2.credentials import Credentials
-import logging
-from db import create_connection, fetch_tokens_from_db, get_credentials_for_user
 
 def sms_reply():
     from app import client, TWILIO_PHONE_NUMBER, check_for_calendar_keyword, generate_response
@@ -14,16 +11,10 @@ def sms_reply():
     
     print(f"User input: {user_input}, Phone number: {phone_number}")  # Debug line
     
-    credentials = get_credentials_for_user(phone_number)
     calendar_keyword_found = check_for_calendar_keyword(user_input, phone_number)
     
     if not calendar_keyword_found:
-        # Check if credentials exist, if not, handle as a simple SMS conversation
-        if credentials:
-            response_text = generate_response(user_input, phone_number, credentials)
-        else:
-            response_text = generate_response(user_input, phone_number)
-        
+        response_text = generate_response(user_input, phone_number)
         message = client.messages.create(
             to=phone_number,
             from_=TWILIO_PHONE_NUMBER,
