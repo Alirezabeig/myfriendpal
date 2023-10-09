11a0228b16da9f8e5aff458479067e47c74979d4
Alireza Beig <alirezabeig@gmail.com>
2023-10-09 08:20:54 -0700

    t

--- truncate_conv.py
+++ truncate_conv.py
@@ -2,19 +2,15 @@
 # truncate_conv.py
 from itertools import chain
 
-
-def truncate_to_last_n_words(conversation, max_words=500):
+def truncate_to_last_n_letters(conversation, max_letters=500):
     # Convert the list of dicts to a list of strings, considering only 'content'.
     text_list = [item['content'] for item in conversation]
     
-    # Join all strings into one and split it into a list of words.
-    all_words = list(chain.from_iterable([text.split() for text in text_list]))
+    # Join all strings into one string.
+    all_letters = ''.join(text_list)
     
-    # Take the last 'max_words' from the list.
-    last_n_words = all_words[-max_words:]
-    
-    # Convert the list of last 'max_words' back to a string.
-    truncated_text = ' '.join(last_n_words)
+    # Take the last 'max_letters' from the string.
+    last_n_letters = all_letters[-max_letters:]
     
     # Initialize an empty list to store truncated conversation.
     truncated_conversation = []
@@ -23,14 +19,14 @@ def truncate_to_last_n_words(conversation, max_words=500):
     pointer = 0
     
     for item in reversed(conversation):
-        # Calculate the length of 'content' for each dict in words.
-        length_in_words = len(item['content'].split())
+        # Calculate the length of 'content' for each dict in letters.
+        length_in_letters = len(item['content'])
         
         # Find the end index for each piece in the truncated_text.
-        end_index = pointer + length_in_words
+        end_index = pointer + length_in_letters
         
         # Extract the substring.
-        part = ' '.join(last_n_words[pointer:end_index])
+        part = last_n_letters[pointer:end_index]
         
         # Create a new dict and append it to the truncated conversation.
         truncated_conversation.append({
