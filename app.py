2d0cea412aba4fc0dc1b68ac2805917bf7e6df2b
Alireza Beig <alirezabeig@gmail.com>
2023-10-09 16:33:49 -0700

    t

--- app.py
+++ app.py
@@ -82,6 +82,8 @@ def generate_response(user_input, phone_number):
         fetch_query = "SELECT conversation_data, google_calendar_email, next_google_calendar_event, refresh_token FROM conversations WHERE phone_number = %s"
         cursor.execute(fetch_query, (phone_number,))
         result = cursor.fetchone()
+        if result:
+            print("****results printed")
 
         # Load existing conversation from database
         if result:
