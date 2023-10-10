de090728fae32a5b2913cf59af3e6e975305aefc
Alireza Beig <alirezabeig@gmail.com>
2023-10-10 08:22:07 -0700

    t

--- truncate_conv.py
+++ truncate_conv.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 
 
-def truncate_to_last_n_letters(text, max = 1000 ):
+def truncate_to_last_n_letters(text, max = 100 ):
     if len(text) > max:
         return text[-max:]
     else:
