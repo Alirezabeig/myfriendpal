723cb0ea823a3e4a4fe00e7f321e377bcda6e3c0
Alireza Beig <alirezabeig@gmail.com>
2023-10-17 10:30:52 -0700

    t

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #app.py
-import requests
 from flask import Flask, request, jsonify, render_template
 from werkzeug.middleware.proxy_fix import ProxyFix
 from twilio.rest import Client
@@ -14,24 +13,16 @@ from twilio_utils import sms_reply
 from google_calendar import oauth2callback
 from truncate_conv import truncate_to_last_n_words
 from shared_utils import get_new_access_token
-from event_utils import fetch_for_prompt_next_calendar
 
 
 import openai
-from psycopg2 import OperationalError, Error
 import traceback
 
 from calendar_utils import get_google_calendar_authorization_url
 from calendar_utils import fetch_google_calendar_info
 
-
-load_dotenv(dotenv_path='./.env')
 app, conn = load_configurations()
 conn = create_connection()
-
-is_loaded = load_dotenv()
-print(f"Is .env loaded: {is_loaded}")
-
 
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
@@ -39,7 +30,6 @@ TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = '+18666421882'
 
-print("Debug in twilio_utils.py: Twilio credentials", os.environ.get('TWILIO_ACCOUNT_SID'), os.environ.get('TWILIO_AUTH_TOKEN'))
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 gpt4_api_key = os.environ.get('GPT4_API_KEY')
@@ -49,6 +39,7 @@ conversations = {}
 logging.basicConfig(level=logging.ERROR)
 
 def check_for_calendar_keyword(user_input, phone_number):
+    print("Checking for calendar keyword...")  # Debug line
     if "calendar" in user_input.lower():
         print("Calendar keyword found. Generating authorization URL...")  # Debug line
         authorization_url = get_google_calendar_authorization_url(phone_number)
@@ -79,39 +70,56 @@ def generate_response(user_input, phone_number):
             logging.info("Database not connected.")
             return "Database error"
 
+        print("Connection:", connection)
+        
         cursor = connection.cursor()
         logging.info('Generate response page accessed')
 
         # Initialize variables
-        current_conversation = []
+        google_calendar_email, refresh_token, next_google_calendar_event, current_conversation = None, None, None, []
 
+        # Fetch existing data from database
+        update_query = ''
         fetch_query = "SELECT conversation_data, google_calendar_email, next_google_calendar_event, refresh_token FROM conversations WHERE phone_number = %s"
         cursor.execute(fetch_query, (phone_number,))
         result = cursor.fetchone()
-        print("Fetching result", result)
+        logging.info(f"Fetched result: {result}")
+        logging.info(f"Phone number being queried: {phone_number}")
 
-        if result is None:
-            # Insert a new row if no conversation exists for this phone number
-            insert_query = "INSERT INTO conversations (phone_number, conversation_data) VALUES (%s, %s);"
-            cursor.execute(insert_query, (phone_number, json.dumps([])))
-            connection.commit()
-        else:
+
+        if result:
             conversation_data, google_calendar_email, next_google_calendar_event, refresh_token = result
-            if next_google_calendar_event:
-                next_google_calendar_event = json.loads(next_google_calendar_event)
+            logging.info(f"Type of conversation_data: {type(conversation_data)}")
+            logging.info(f"Vaxing of conversation_data before if-statement: {conversation_data}")
+
+            # Ensure conversation_data is a JSON-formatted string
+            if not isinstance(conversation_data, str):
+                try:
+                    conversation_data = json.dumps(conversation_data)
+                except (TypeError, ValueError) as e:
+                    logging.error(f"Anxing error occurred while serializing: {e}")
+
+            # Deserialize JSON string into Python list
             if isinstance(conversation_data, str):
-                current_conversation = json.loads(conversation_data)
+                try:
+                    current_conversation = json.loads(conversation_data)
+                    logging.info(f"Oldix Conversations Loaded: {current_conversation}")
+                except json.JSONDecodeError as e:
+                    logging.error(f"An error occurred while deserializing: {e}")
+
+        else: 
+            current_conversation = conversation_data
+            logging.info("No past conversations found.")
 
         if google_calendar_email:
-            next_google_calendar_event = fetch_for_prompt_next_calendar(refresh_token)
-            current_conversation.append({"role": "system", "content": "Fetched new calendar events"})
-            serialized_next_google_calendar_event = json.dumps(next_google_calendar_event)
-            cursor.execute("UPDATE conversations SET next_google_calendar_event = %s WHERE phone_number = %s;", (serialized_next_google_calendar_event, phone_number))
+            next_google_calendar_event = fetch_next_calendar_event(refresh_token)
+            cursor.execute("UPDATE conversations SET next_google_calendar_event = %s WHERE phone_number = %s;", (next_google_calendar_event, phone_number))
             connection.commit()
 
-        new_conversation_system = {
+        current_conversation = truncate_to_last_n_words(current_conversation, 500)
+        new_conversation_system = { 
             "role": "system",
-            "content": "Do this role play always. Your name is Pal, your were made by one engineer with love and passion and tons of coding skills. You are concise up to 50 words max. Always friendly, no need to list calendar events unless you are asked, learn about them"
+            "content": "Do this role play always. Your name is Pal, your were made by one engineer with love and passion and tons of coding skills. You are concise up to 30 words max. Always friendly, no need to list calendar events unless you are asked, learn about them"
         }
         new_conversation_user = {
             "role": "user",
@@ -120,15 +128,19 @@ def generate_response(user_input, phone_number):
         
         current_conversation.extend([new_conversation_system, new_conversation_user])
 
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[{
-                'role': msg['role'],
-                'content': msg['content']} for msg in current_conversation]
-        )
+        if google_calendar_email and next_google_calendar_event:
+            new_conversation_calendar = {
+                "role": "system",
+                "content": f"User's email is {google_calendar_email}. Next event is {next_google_calendar_event}."
+            }
+            current_conversation.append(new_conversation_calendar)
 
+            logging.info(f"current_conversation: {current_conversation}")
+
+        
+        
+        response = openai.ChatCompletion.create(model="gpt-4", messages=current_conversation)
         gpt4_reply = response['choices'][0]['message']['content'].strip()
-        gpt4_reply = ' '.join(gpt4_reply.split()[:30])
 
         new_conversation_assistant = {
             "role": "assistant",
@@ -137,19 +149,22 @@ def generate_response(user_input, phone_number):
         current_conversation.append(new_conversation_assistant)
 
         updated_data = json.dumps(current_conversation)
-        
+
         update_query = "UPDATE conversations SET conversation_data = %s WHERE phone_number = %s;"
-        logging.info(f"Executing SQL Query: {update_query} with phone_number = {phone_number}")  # Log the SQL query
         cursor.execute(update_query, (updated_data, phone_number))
         connection.commit()
-        logging.info("Transaction committed.")  
+
+        # Fetch data immediately after updating, for debugging purposes
+        cursor.execute(fetch_query, (phone_number,))
+        post_update_result = cursor.fetchone()
+        logging.info(f"Post-update fetched result: {post_update_result}")
 
         return gpt4_reply
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
+        logging.error(traceback.format_exc())
         return "Sorry, I couldn't understand that."
-
 
 @app.route("/sms", methods=['POST'])
 def handle_sms():
@@ -190,13 +205,6 @@ def handle_oauth2callback():
     print("Entered handle_oauth2callback in app.py")
     return oauth2callback()
 
-@app.route('/privacy_policy')
-def privacy_policy():
-    return render_template('privacy_policy.html')
-
-@app.route('/terms_of_service')
-def terms_of_service():
-    return render_template('terms_of_service.html')
 
 @app.route('/pal', methods=['GET'])
 def pal_page():
@@ -208,4 +216,3 @@ if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5002))
     app.run(host="0.0.0.0", port=port)
-
