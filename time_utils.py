50ec8f6ba21ed6c5e686ba7609eae6e353450d7c
Alireza Beig <alirezabeig@gmail.com>
2023-10-23 07:52:47 -0700

    t

--- time_utils.py
+++ time_utils.py
@@ -1,18 +1,27 @@
 
-#time_utils.py
 from datetime import datetime
 import requests
 import pytz
-from datetime import datetime
-
-current_time = datetime.now()
 
 def get_local_time(ip_address):
-    response = requests.get(f'http://ip-api.com/json/{ip_address}')
+    try:
+        response = requests.get(f'http://ip-api.com/json/{ip_address}')
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
+    except requests.RequestException as e:
+        print(f"Failed to get data from ip-api.com: {e}")
+        return None
+
     data = response.json()
     timezone = data.get('timezone')
+
     if timezone:
-        tz = pytz.timezone(timezone)
-        current_time = datetime.now(tz)
-        return current_time
-    return None
+        try:
+            tz = pytz.timezone(timezone)
+            current_time = datetime.now(tz)
+            return current_time
+        except pytz.UnknownTimeZoneError:
+            print(f"Unknown timezone: {timezone}")
+            return None
+    else:
+        print("Timezone not found in the API response.")
+        return None
