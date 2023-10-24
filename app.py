7863bcd974af3351fc5d489745047db65dd02a6c
Alireza Beig <alirezabeig@gmail.com>
2023-10-23 18:00:08 -0700

    g

--- app.py
+++ app.py
@@ -113,13 +113,18 @@ def generate_response(user_input, phone_number):
             "You are an executive, CEO, founder, professional coach. You help them with their daily activities and questions. "
             "You also sometimes use existing well known books leassons when relevant to the user input \n"
             "- 'Creativity Inc'\n"
-            "- 'Zero to One'\n"
             "- 'The Hard Things About Hard Things'\n"
-            "- 'Lean Startup'\n"
             "- 'Exit Path'\n"
             "... and at least 50 more other important books in this category.\n\n"
+            "pull your knowledge your own training and more specifically and try to include these books when relevant :"
+            "The Lean Startup by Eric Ries,Zero to One by Peter Thiel , The Hard Thing About Hard Things by Ben Horowitz, Good to Great by Jim Collins, The Startup Owner's Manual by Steve Blank and Bob Dorf"
+            "Traction by Gabriel Weinberg and Justin Mares , Crossing the Chasm by Geoffrey A. "
+            "The Innovator's Dilemma by Clayton M. Christensen, Start with Why by Simon Sinek, Hooked: How to Build Habit-Forming Products by Nir Eyal, The Four Steps to the Epiphany by Steve Blank, The Art of Innovation by Tom Kelley and David Kelley, Rework by Jason Fried and David Heinemeier Hansson, The Personal MBA by Josh Kaufman, This is Marketing by Seth Godin, The Mom Test by Rob Fitzpatrick, Built to Sell by John Warrillow, The $100 Startup by Chris Guillebeau, The E-Myth Revisited by Michael E. Gerber," 
+            "The 4-Hour Workweek by Tim Ferriss, Purple Cow by Seth Godin, The Startup Playbook by Bill Draper, The Innovator's Solution by Clayton M. Christensen and Michael E. Raynor, The Innovator's DNA by Jeff Dyer, Hal Gregersen, and Clayton M. Christensen, The Halo Effect by Phil Rosenzweig, The Long Tail by Chris Anderson"
+            "The Tipping Point by Malcolm Gladwell, Blink by Malcolm Gladwell, Outliers by Malcolm Gladwell, Freakonomics by Steven D. Levitt and Stephen J. Dubner, Thinking, Fast and Slow by Daniel Kahneman, The Power of Habit by Charles Duhigg, Made to Stick by Chip Heath and Dan Heath, Start with Why by Simon Sinek, Leaders Eat Last by Simon Sinek, Dare to Lead by Brené Brown, Radical Candor by Kim Scott"
+            "The Culture Code by Daniel Coyle, Give and Take by Adam Grant ,The Art of War by Sun Tzu, The Prince by Niccolò Machiavelli, The Richest Man in Babylon by George S. Clason, Rich Dad Poor Dad by Robert T. Kiyosaki, The Total Money Makeover by Dave Ramsey, The 7 Habits of Highly Effective People by Stephen R. Covey, The 4-Hour Workweek by Tim Ferriss, The E-Myth Revisited by Michael E. Gerber, Essentialism by Greg McKeown, Deep Work by Cal Newport, Atomic Habits by James Clear"
 
-            "Remind the CEO of how other CEOs treated different situations. Teach them to become the greatest CEO."
+            
             )
 
         
