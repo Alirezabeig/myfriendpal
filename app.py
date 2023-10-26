35f313d8d17ecbd41f989e26ab701613c4c86035
Alireza Beig <alirezabeig@gmail.com>
2023-10-26 09:03:25 -0700

    t

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def generate_response(user_input=None, phone_number=None):
         gpt4_reply = response['choices'][0]['message']['content'].strip()
         gpt4_reply = gpt4_reply[:1600] 
 
-        
+        current_conversation.pop(-1) 
         
         print("curre_conv2", current_conversation)
 
