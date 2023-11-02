5a33cd0935b852b94ec394db956264e7de2a663e
Alireza Beig <alirezabeig@gmail.com>
2023-11-02 16:21:31 -0700

    t

--- google_calendar.py
+++ google_calendar.py
@@ -1,29 +1,23 @@
-# google_calender.py
-from flask import request
-import requests
+# google_calendar.py
+from aiohttp import web, ClientSession
 import os
-from db import conn, create_connection
-from calendar_utils import fetch_google_calendar_info
-from flask import render_template
 import json
+from db import create_connection
+from calendar_utils import fetch_google_calendar_info
 from twilio_utils import send_sms_confirmation
+import asyncio
 
 from dotenv import load_dotenv
 load_dotenv()
 
-# Rest of your code
-
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = '+18666421882'
-conn = create_connection()
 
-def oauth2callback():
+async def oauth2callback(request):
     print("Entered oauth2callback")
-    auth_code = request.args.get('code')
-    phone_number = request.args.get('state')  # Assuming you passed phone_number as 'state' during OAuth2 initiation
+    auth_code = request.query.get('code')
+    phone_number = request.query.get('state')  # Assuming you passed phone_number as 'state' during OAuth2 initiation
 
     token_data = {
         'client_id': GOOGLE_CLIENT_ID,
@@ -33,54 +27,46 @@ def oauth2callback():
         'grant_type': 'authorization_code'
     }
 
-    response = requests.post('https://oauth2.googleapis.com/token', data=token_data)
-    token_info = response.json()
+    async with ClientSession() as session:
+        async with session.post('https://oauth2.googleapis.com/token', data=token_data) as response:
+            token_info = await response.json()
+            access_token = token_info.get('access_token')
+            refresh_token = token_info.get('refresh_token')
 
-    # Check if we have valid tokens
-    access_token = token_info.get('access_token')
-    refresh_token = token_info.get('refresh_token')
+            if access_token and refresh_token:
+                google_calendar_email, next_google_calendar_event, local_now = await fetch_google_calendar_info(access_token, refresh_token)
+                
+                connection = await create_connection()
 
-    if access_token and refresh_token:
-        # Fetch and store Gmail ID and next Google Calendar event
-        google_calendar_email, next_google_calendar_event , local_now = fetch_google_calendar_info(access_token, refresh_token)
-        
-        # Check if connection is closed
-        if conn.closed:
-            print("Connection closed, re-opening...")
-            # Re-open your connection here
-            # conn = psycopg2.connect( ... )
-        
-        # Update the database
-        with conn.cursor() as cursor:  # Using 'with' ensures the cursor will be closed after use
-            update_query = '''UPDATE conversations SET oauth_token = %s, google_calendar_email = %s, local_now = %s , next_google_calendar_event = %s, refresh_token = %s WHERE phone_number = %s;'''
-            try:
-                cursor.execute(update_query, (json.dumps(token_info), google_calendar_email, next_google_calendar_event, local_now, refresh_token, phone_number))
-                conn.commit()
-            except Exception as e:
-                print(f"Error occurred: {e}")
-                conn.rollback()
-        send_sms_confirmation(phone_number, "Give me 3 minutes to sync with your Google Calendar and Gmail. Your accounts have been successfully authorized. I will be ready in just a bit.")
-        
-    else:
-        print("Failed to get access_token or refresh_token")
-        send_sms_confirmation(phone_number, "Failed to authorize Google Calendar.")
+                update_query = '''UPDATE conversations SET oauth_token = $1, google_calendar_email = $2, local_now = $3, next_google_calendar_event = $4, refresh_token = $5 WHERE phone_number = $6;'''
+                
+                try:
+                    await connection.execute(update_query, json.dumps(token_info), google_calendar_email, local_now, next_google_calendar_event, refresh_token, phone_number)
+                except Exception as e:
+                    print(f"Error occurred: {e}")
+                    await connection.rollback()
+                finally:
+                    await connection.close()
+                
+                await send_sms_confirmation(phone_number, "Your accounts have been successfully authorized.")
+            else:
+                print("Failed to get access_token or refresh_token")
+                await send_sms_confirmation(phone_number, "Failed to authorize Google Calendar.")
 
-    return render_template('authorization_complete.html')
+    return web.Response(text="Authorization complete", content_type="text/html")
 
-    
-def get_new_access_token(refresh_token):
+async def get_new_access_token(refresh_token):
     data = {
         'client_id': GOOGLE_CLIENT_ID,
         'client_secret': GOOGLE_CLIENT_SECRET,
         'refresh_token': refresh_token,
         'grant_type': 'refresh_token'
     }
-    try:
-        response = requests.post('https://oauth2.googleapis.com/token', data=data)
-        token_info = response.json()
-        new_access_token = token_info.get('access_token')
-        return new_access_token
-    except Exception as e:
-        logging.error(f"Failed to get new access token: {e}")
-        return None
+    
+    async with ClientSession() as session:
+        async with session.post('https://oauth2.googleapis.com/token', data=data) as response:
+            token_info = await response.json()
+            new_access_token = token_info.get('access_token')
+            return new_access_token
 
+# Rest of your code
